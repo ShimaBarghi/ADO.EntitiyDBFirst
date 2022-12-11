9046c31e07e459ac403768a3e1c19ffd080926db
unity <User@User9.iau.org>
2022-12-11 12:33:30 +0330

    first commit

---
+++ First_App/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace First_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ViewResult ContactUs()
+        {
+            return View("ContactUs");
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult telefon()
+        {
+            return View();
+        }
+        public ActionResult zendegi()
+        {
+            List<string> names = new List<string>();
+            names.Add("shima barghi");
+            names.Add("homa barghi");
+            ViewBag.name = names;
+
+
+
+
+            return View();
+        }
+
+        
+      
+    }
+}
---
+++ First_App/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace First_App.Controllers
+{
+    public class TestController : Controller
+    {
+        public string Hello()
+        {
+            return "hello girl ...";
+        }
+    }
+}
